fe0e9f354b7ddcbfadbb6b81194b89eef0181e42
Notsoworkingbro <workingamap@gmail.com>
2025-04-27 16:12:25 +0800

    other classes

---
+++ instances.py
@@ -0,0 +1,25 @@
+class basic_unit_stats:
+    __slots__ = [
+        'name', 
+        'health', 
+        'armor', 
+        'attack', 
+        'attack_speed',
+
+        'img'
+        ]
+
+    def __init__(self, 
+                 
+                 name : str,
+                 health : int,
+                 attack : int,
+                 attack_speed : int,
+                 
+                 img : str
+                 ):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.attack_speed = attack_speed
+        self.img = img
